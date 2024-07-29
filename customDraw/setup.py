797017d24e5fffdd90cf61191aa87c0856471dd9
Quatn <nguyenminhquanvh@gmail.com>
2024-07-29 15:33:46 +0700

    Project refactoring

--- customDraw/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-modl = Extension("customDraw", sources=["customDraw.c"])
-
-setup(name="PackageName",
-      version="1",
-      description="Draws Stuffs",
-      ext_modules=[modl]
-      )
